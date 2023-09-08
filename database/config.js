d17e1efb93596f7482e4f679b22240015359fbc0
isaactaclia <icolmenares@taclia.com>
2023-09-08 11:51:29 +0200

    Fin sección 10 - Auth

--- database/config.js
+++ database/config.js
@@ -8,12 +8,12 @@ const dbConnection = async() => {
         await mongoose.connect(process.env.DB_CNN);
 
         console.log('DB Online');
+    
 
     }catch( error ){
         console.log(error);
         throw new Error('Error a la hora de iniciar la BD ver logs')
     }
-
 
 }
 module.exports = {
--- index.js
+++ index.js
@@ -10,18 +10,17 @@ const app = express();
 //Configurar CORS
 app.use( cors() );
 
+//Lectura y parseo del body
+app.use( express.json() );
+
 //Base de datos
 dbConnection();
 
 //Rutas
-app.get( '/', (req, res) => {
+app.use( '/api/usuarios', require('./routes/usuarios') );
+app.use( '/api/login', require('./routes/auth') );
 
-    res.json({
-        ok: true,
-        mgs: 'Hola Mundo'
-    })
 
-} );
 
 app.listen( process.env.PORT, () =>{
     console.log('Servidor corriendo en puerto ' + process.env.PORT);
